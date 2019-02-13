1ccd37d4c985b6a83810a83e3fa041c0068f6180
romanse <romanse@playtika.com>
2019-02-13 23:51:16 +0300

    initial commit

---
+++ src/main/scala/observatory/Extraction.scala
@@ -0,0 +1,41 @@
+package observatory
+
+import java.time.LocalDate
+import java.nio.file.Paths
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+
+/**
+  * 1st milestone: data extraction
+  */
+object Extraction {
+
+  def getSpark(): SparkSession = {
+      SparkSession
+        .builder()
+        .appName("Observatory")
+        .config("spark.master", "local")
+        .getOrCreate()
+  }
+
+  /**
+    * @param year             Year number
+    * @param stationsFile     Path of the stations resource file to use (e.g. "/stations.csv")
+    * @param temperaturesFile Path of the temperatures resource file to use (e.g. "/1975.csv")
+    * @return A sequence containing triplets (date, location, temperature)
+    */
+  def locateTemperatures(year: Year, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Temperature)] = {
+    val sc = getSpark()
+    ???
+  }
+
+  /**
+    * @param records A sequence containing triplets (date, location, temperature)
+    * @return A sequence containing, for each location, the average temperature over the year.
+    */
+  def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Temperature)]): Iterable[(Location, Temperature)] = {
+    ???
+  }
+
+}
